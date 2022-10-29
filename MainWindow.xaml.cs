b420361d872e5fecdc1f9e5454260bca979188ef
zƃɐıds <agusspinelli.dev@gmail.com>
2022-10-29 00:21:19 -0300

    Basic frontend

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AppPage main = new();
+            this.Content = main;
+        }
     }
 }
